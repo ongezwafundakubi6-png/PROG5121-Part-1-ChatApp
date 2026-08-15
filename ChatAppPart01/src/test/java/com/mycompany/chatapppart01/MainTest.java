e3bc0c2f3992189781851c67c850d8932bc0cde9
ongezwafundakubi6-png <ongezwafundakubi6@gmail.com>
2026-08-15 11:40:34 +0200

    Added login test and main

---
+++ ChatAppPart01/src/test/java/com/mycompany/chatapppart01/MainTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.chatapppart01;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
